af7864e6618f69dd4da3b3c8b6586fe30820dfcd
Eliah7 <Guest@eliahs-mac-2.local>
2018-08-30 10:30:22 +0300

    Initial Commit

---
+++ My Accountant/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  My Accountant
+//
+//  Created by Guest User on 8/30/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
