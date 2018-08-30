e6e2e3609705000c9aea30e662c8f6680f4de843
Eliah7 <Guest@Eliahs-mac-2.local>
2018-08-30 10:46:14 +0300

    Basic iOS app Project Setup

--- My Accountant/ViewController.swift
+++ My Accountant/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var clickBtn: UIButton!
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    var txt:String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +25,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonClicked2(_ sender: Any) {
+        buttonClicked()
+    }
+    
+    func buttonClicked() {
+       txt = textField.text!
+        textView.text = txt
+    }
 }
 
